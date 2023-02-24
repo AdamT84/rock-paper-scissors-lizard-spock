8177ecbe429ffdebf0cdbdc4d3394f38e3182a81
Adam Toman <adamtoman1@aol.com>
2023-02-24 22:04:47 +0000

    Add comments to functions

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,33 +3,29 @@ document.addEventListener("DOMContentLoaded", function() {
 
   for (let button of buttons) {
     button.addEventListener("click", function() {
-      if (this.getAttribute("data-index") === "0") {
-        alert("You clicked rock!");
-      } else if (this.getAttribute("data-index") === "1") {
-        alert("You clicked paper!");
-      } else if (this.getAttribute("data-index") === "2") {
-        alert("You clicked Scissors!");
-      } else if (this.getAttribute("data-index") === "3") {
-        alert("You clicked lizard!");
-      } else if (this.getAttribute("data-index") === "4") {
-        alert("You clicked spock!");
-      }
-    })
-  }
+      let playerChoice = this.getAttribute("data-index");
+      runGame(playerChoice);
+    });
+  }  
 })
 
-function runGame() {
-  
+function runGame(playerChoice) {
+  let computerChoice = Math.floor(Math.random() * 5);
+  let result = checkWinner(choices[computerChoice], choices[playerChoice]);
+
+  updateScores(result);
 }
 
-function getResult() {
 
-}
 
-function playerScore() {
-
-}
-
-function computerScore() {
-
+if (this.getAttribute("data-index") === "0") {
+  alert("You clicked rock!");
+} else if (this.getAttribute("data-index") === "1") {
+  alert("You clicked paper!");
+} else if (this.getAttribute("data-index") === "2") {
+  alert("You clicked Scissors!");
+} else if (this.getAttribute("data-index") === "3") {
+  alert("You clicked lizard!");
+} else if (this.getAttribute("data-index") === "4") {
+  alert("You clicked spock!");
 }
