624c54dda9761bdf688958e48ad749dcc01997bf
Adam Toman <adamtoman1@aol.com>
2023-02-24 13:31:39 +0000

    Add robot choice and cycle though

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,95 +1,62 @@
-const machineScore = document.getElementById('machine-score');
-const humanScore = document.getElementById('human-score');
-const buttons = document.getElementsByClassName('btn');
-const playerOptions = ["rock", "paper", "scissors", "lizard", "spock"]
-
-for (let button of buttons) {
-    button.addEventListener("click", function() {
-        let humanChoice = this.getAttribute("data-type");
-        let machineChoice = Math.floor(Math.random() * 5);
-    })
-}
+const robotSelection = document.getElementById('robot-choice');
+const yourSelection = document.getElementById('your-choice');
+const resultDisplay = document.getElementById('result');
+const playerOptions = document.getElementsByClassName('btn');
+let yourChoice
+let robotChoice
+let result
 
 
-let rules = {
-    Rock: {
-        Rock: 'draw',
-        Scissors: 'lose',
-        Paper: 'win',
-        Spock: 'win',
-        Lizard: 'lose'
-    },
-    Paper: {
-        Paper: 'draw',
-        Rock: 'lose',
-        Scissors: 'win',
-        Lizard: 'win',
-        Spock: 'lose'
-    },
-    Scissors: {
-        Scissors: 'draw',
-        Paper: 'lose',
-        Rock: 'win',
-        Lizard: 'lose',
-        Spock: 'win'
-    },
-    Lizard: {
-        Scissors: 'win',
-        Paper: 'lose',
-        Rock: 'win',
-        Lizard: 'draw',
-        Spock: 'lose'
-    },
-    Spock: {
-        Scissors: 'lose',
-        Paper: 'win',
-        Rock: 'lose',
-        Lizard: 'win',
-        Spock: 'draw'
+playerOptions.forEach(playerOptions => playerOptions.addEventListener('click', (e) => {
+    yourChoice = e.target.id
+    yourSelection.innerHTML = userChoice
+    generaterobotChoice()
+    getResult()
+  }))
+  
+  function generaterobotChoice() {
+    const randomNumber = Math.floor(Math.random() * 5) 
+    
+    if (randomNumber === 0) {
+      robotChoice = 'rock'
     }
-}
-
-function winner() {
-    let result = document.querySelector('.result');
-    let playerScoreBoard = document.querySelector('.p-count');
-    let computerScoreBoard = document.querySelector('.c-count');
-    player = player.toLowerCase();
-    computer = computer.toLowerCase();
-    if(player === computer){
-        result.textContent = 'Tie'
+    if (randomNumber === 1) {
+      robotChoice = 'scissors'
     }
-    else if(player == 'rock'){
-        if(computer == 'paper'){
-            result.textContent = 'Computer Won';
-            computerScore++;
-            computerScoreBoard.textContent = computerScore;
-
-        }else{
-            result.textContent = 'Player Won'
-            playerScore++;
-            playerScoreBoard.textContent = playerScore;
-        }
+    if (randomNumber === 2) {
+      robotChoice = 'paper'
     }
-    else if(player == 'scissors'){
-        if(computer == 'rock'){
-            result.textContent = 'Computer Won';
-            computerScore++;
-            computerScoreBoard.textContent = computerScore;
-        }else{
-            result.textContent = 'Player Won';
-            playerScore++;
-            playerScoreBoard.textContent = playerScore;
-        }
+    if (randomNumber === 3) {
+        robotChoice = 'lizard'
+      }
+      if (randomNumber === 2) {
+        robotChoice = 'spock'
+      }
+      
+    robotSelection.innerHTML = computerChoice
+  }
+  
+  function getResult() {
+    if (computerChoice === userChoice) {
+      result = 'its a draw!'
     }
-    else if(player == 'paper'){
-        if(computer == 'scissors'){
-            result.textContent = 'Computer Won';
-            computerScore++;
-            computerScoreBoard.textContent = computerScore;
-        }else{
-            result.textContent = 'Player Won';
-            playerScore++;
-            playerScoreBoard.textContent = playerScore;
-        }
+    if (computerChoice === 'rock' && userChoice === "paper") {
+      result = 'you win!'
     }
-}
+    if (computerChoice === 'rock' && userChoice === "scissors") {
+      result = 'you lost!'
+    }
+    if (computerChoice === 'paper' && userChoice === "scissors") {
+      result = 'you win!'
+    }
+    if (computerChoice === 'paper' && userChoice === "rock") {
+      result = 'you lose!'
+    }
+    if (computerChoice === 'scissors' && userChoice === "rock") {
+      result = 'you win!'
+    }
+    if (computerChoice === 'scissors' && userChoice === "paper") {
+      result = 'you lose!'
+    }
+    resultDisplay.innerHTML = result
+  }
