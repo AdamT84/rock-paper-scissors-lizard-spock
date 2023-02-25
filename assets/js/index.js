80a7f751e13ba0aed8969dde06deea3417ff02b3
Adam Toman <adamtoman1@aol.com>
2023-02-25 00:31:30 +0000

    Add style to game area

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,31 +1,106 @@
-document.addEventListener("DOMContentLoaded", function() {
-  let buttons = document.getElementsByTagName("button");
+// List all DOM variables //
+const compChoiceDisplay = document.getElementById("comp-choice")
+const playerChoiceDisplay = document.getElementById("player-choice")
+const resultDisplay = document.getElementById("result")
+const possibleChoices = document.querySelectorAll("button")
+let playerChoice
+let compChoice
+let result
 
-  for (let button of buttons) {
-    button.addEventListener("click", function() {
-      let playerChoice = this.getAttribute("data-index");
-      runGame(playerChoice);
-    });
-  }  
-})
+// Add event listener to buttons //
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+  playerChoice = e.target.id
+  playerChoiceDisplay.innerHTML = playerChoice
+  generateCompChoice()
+  getResult()
+}))
 
-function runGame(playerChoice) {
-  let computerChoice = Math.floor(Math.random() * 5);
-  let result = checkWinner(choices[computerChoice], choices[playerChoice]);
-
-  updateScores(result);
+// Function to generate the computers choice using random number //
+function generateCompChoice() {
+  const computerOption = Math.floor(Math.random() * 5) 
+  
+  if (computerOption === 0) {
+    compChoice = "Rock"
+  }
+  if (computerOption === 1) {
+    compChoice = "Scissors"
+  }
+  if (computerOption === 2) {
+    compChoice = "Paper"
+  }
+  if (computerOption === 3) {
+    compChoice = "Lizard"
+  }
+  if (computerOption === 4) {
+    compChoice = "Spock"
+  }
+  compChoiceDisplay.innerHTML = compChoice
 }
 
-
-
-if (this.getAttribute("data-index") === "0") {
-  alert("You clicked rock!");
-} else if (this.getAttribute("data-index") === "1") {
-  alert("You clicked paper!");
-} else if (this.getAttribute("data-index") === "2") {
-  alert("You clicked Scissors!");
-} else if (this.getAttribute("data-index") === "3") {
-  alert("You clicked lizard!");
-} else if (this.getAttribute("data-index") === "4") {
-  alert("You clicked spock!");
+// Gets the result by comparing all possible outcomes //
+function getResult() {
+  if (compChoice === playerChoice) {
+    result = "It's a draw!"
+  }
+  if (compChoice === "Rock" && playerChoice === "Paper") {
+    result = "You win!"
+  }
+  if (compChoice === "Rock" && playerChoice === "Scissors") {
+    result = "You lost!"
+  }
+  if (compChoice === "Rock" && playerChoice === "Lizard") {
+    result = "You lose!"
+  }
+  if (compChoice === "Rock" && playerChoice === "Spock") {
+    result = "You win!"
+  }
+  if (compChoice === "Paper" && playerChoice === "scissors") {
+    result = "You win!"
+  }
+  if (compChoice === "Paper" && playerChoice === "Lizard") {
+    result = "You win!"
+  }
+  if (compChoice === "Paper" && playerChoice === "Spock") {
+    result = "You lose!"
+  }
+  if (compChoice === "Paper" && playerChoice === "Rock") {
+    result = "You lose!"
+  }
+  if (compChoice === "Scissors" && playerChoice === "Rock") {
+    result = "You win!"
+  }
+  if (compChoice === "Scissors" && playerChoice === "Paper") {
+    result = "You lose!"
+  }
+  if (compChoice === "Scissors" && playerChoice === "Lizard") {
+    result = "You lose!"
+  }
+  if (compChoice === "Scissors" && playerChoice === "Spock") {
+    result = "You win!"
+  }
+  if (compChoice === "Lizard" && playerChoice === "Spock") {
+    result = "You lose!"
+  }
+  if (compChoice === "Lizard" && playerChoice === "Rock") {
+    result = "You win!"
+  }
+  if (compChoice === "Lizard" && playerChoice === "Paper") {
+    result = "You lose!"
+  }
+  if (compChoice === "Lizard" && playerChoice === "Scissors") {
+    result = "You win!"
+  }
+  if (compChoice === "Spock" && playerChoice === "Rock") {
+    result = "You lose!"
+  }
+  if (compChoice === "Spock" && playerChoice === "Paper") {
+    result = "You win!"
+  }
+  if (compChoice === "Spock" && playerChoice === "Scissors") {
+    result = "You lose!"
+  }
+  if (compChoice === "Spock" && playerChoice === "Lizard") {
+    result = "You win!"
+  }
+  resultDisplay.innerHTML = result
 }
