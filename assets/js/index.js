f7400d2c42dba7a1a357d4d801c6b4b3be2aa816
Adam Toman <adamtoman1@aol.com>
2023-02-25 20:59:23 +0000

    Adding reset button to allow game to be reset

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,106 +1,142 @@
 // List all DOM variables //
-const compChoiceDisplay = document.getElementById("comp-choice")
-const playerChoiceDisplay = document.getElementById("player-choice")
-const resultDisplay = document.getElementById("result")
-const possibleChoices = document.querySelectorAll("button")
-let playerChoice
-let compChoice
-let result
+const compChoiceDisplay = document.getElementById("comp-choice");
+const playerChoiceDisplay = document.getElementById("player-choice");
+const resultDisplay = document.getElementById("result");
+const possibleChoices = document.querySelectorAll("img");
+const computerScore = document.getElementById("comp-score");
+const playerScore = document.getElementById("player-score");
+const tiedGame = document.getElementById("tied-game");
+
+// Variables for player selection, computer section and result //
+let playerChoice;
+let compChoice;
+let result;
+
+// Variables to hold scores //
+let humanScore = "0";
+let botScore = "0";
+let tieScore ="0";
 
 // Add event listener to buttons //
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
-  playerChoice = e.target.id
-  playerChoiceDisplay.innerHTML = playerChoice
-  generateCompChoice()
-  getResult()
-}))
-
+  playerChoice = e.target.id;
+  playerChoiceDisplay.innerHTML = playerChoice;
+  generateCompChoice();
+  getResult();
+  getScore();
+  endGame();
+  restartGame();
+  restartScores();
+}));
+  
 // Function to generate the computers choice using random number //
 function generateCompChoice() {
-  const computerOption = Math.floor(Math.random() * 5) 
+  const computerOption = Math.floor(Math.random() * 5);
   
   if (computerOption === 0) {
-    compChoice = "Rock"
+    compChoice = "Rock";
   }
   if (computerOption === 1) {
-    compChoice = "Scissors"
+    compChoice = "Scissors";
   }
   if (computerOption === 2) {
-    compChoice = "Paper"
+    compChoice = "Paper";
   }
   if (computerOption === 3) {
-    compChoice = "Lizard"
+    compChoice = "Lizard";
   }
   if (computerOption === 4) {
-    compChoice = "Spock"
+    compChoice = "Spock";
   }
-  compChoiceDisplay.innerHTML = compChoice
+  compChoiceDisplay.innerHTML = compChoice;
 }
 
 // Gets the result by comparing all possible outcomes //
 function getResult() {
+
+  let won = "You win!";
+  let lost = "You lose!";
+  let tie = "It's a tie!";
+
   if (compChoice === playerChoice) {
-    result = "It's a draw!"
+    result = tie;
+
+  } else if (compChoice === "Rock" && playerChoice === "Paper") {
+    result = won;
+     
+  } else if (compChoice === "Rock" && playerChoice === "Spock") {
+    result = won;
+     
+  } else if (compChoice === "Paper" && playerChoice === "Scissors") {
+    result = won;
+     
+  } else if (compChoice === "Paper" && playerChoice === "Lizard") {
+    result = won;
+     
+  } else if (compChoice === "Scissors" && playerChoice === "Rock") {
+    result = won;
+     
+  } else if (compChoice === "Scissors" && playerChoice === "Spock") {
+    result = won;
+     
+  } else if (compChoice === "Lizard" && playerChoice === "Rock") {
+    result = won;
+     
+  } else if (compChoice === "Lizard" && playerChoice === "Scissors") {
+    result = won;
+
+  } else if (compChoice === "Spock" && playerChoice === "Paper") {
+    result = won;
+
+  } else if (compChoice === "Spock" && playerChoice === "Lizard") {
+    result = won;
+  } else {
+    result = lost;
   }
-  if (compChoice === "Rock" && playerChoice === "Paper") {
-    result = "You win!"
+
+  // Increments the score depending on outcome //
+  resultDisplay.innerHTML = result;
+  if (result === won) {
+    humanScore++;
+  } else if (result === tie) {
+    tieScore++;
+  } else {
+    botScore++;
   }
-  if (compChoice === "Rock" && playerChoice === "Scissors") {
-    result = "You lost!"
+  
+  // Writes scores to HTML elements //
+  playerScore.innerHTML = humanScore;
+  computerScore.innerHTML = botScore;
+  tiedGame.innerHTML = tieScore;
+
+  if (humanScore === 10) {
+    resultDisplay.innerHTML = "GAME OVER!! Congratualations you've won the game, press the reset button to start again";
+  } else if (botScore === 10) {
+    resultDisplay.innerHTML = "GAME OVER!! Bad luck, you didn't win, press the reset button to try again";
   }
-  if (compChoice === "Rock" && playerChoice === "Lizard") {
-    result = "You lose!"
+
+  if (humanScore === 10 || botScore === 10){
+    endGame();
   }
-  if (compChoice === "Rock" && playerChoice === "Spock") {
-    result = "You win!"
-  }
-  if (compChoice === "Paper" && playerChoice === "scissors") {
-    result = "You win!"
-  }
-  if (compChoice === "Paper" && playerChoice === "Lizard") {
-    result = "You win!"
-  }
-  if (compChoice === "Paper" && playerChoice === "Spock") {
-    result = "You lose!"
-  }
-  if (compChoice === "Paper" && playerChoice === "Rock") {
-    result = "You lose!"
-  }
-  if (compChoice === "Scissors" && playerChoice === "Rock") {
-    result = "You win!"
-  }
-  if (compChoice === "Scissors" && playerChoice === "Paper") {
-    result = "You lose!"
-  }
-  if (compChoice === "Scissors" && playerChoice === "Lizard") {
-    result = "You lose!"
-  }
-  if (compChoice === "Scissors" && playerChoice === "Spock") {
-    result = "You win!"
-  }
-  if (compChoice === "Lizard" && playerChoice === "Spock") {
-    result = "You lose!"
-  }
-  if (compChoice === "Lizard" && playerChoice === "Rock") {
-    result = "You win!"
-  }
-  if (compChoice === "Lizard" && playerChoice === "Paper") {
-    result = "You lose!"
-  }
-  if (compChoice === "Lizard" && playerChoice === "Scissors") {
-    result = "You win!"
-  }
-  if (compChoice === "Spock" && playerChoice === "Rock") {
-    result = "You lose!"
-  }
-  if (compChoice === "Spock" && playerChoice === "Paper") {
-    result = "You win!"
-  }
-  if (compChoice === "Spock" && playerChoice === "Scissors") {
-    result = "You lose!"
-  }
-  if (compChoice === "Spock" && playerChoice === "Lizard") {
-    result = "You win!"
-  }
-  resultDisplay.innerHTML = result
+
 }
+function endGame() {
+  possibleChoices.disabled = true;
+}
+
+function restartGame() {
+  restartScores();
+  possibleChoices.disabled = false;
+}
+
+function restartScores() {
+  humanScore = 0;
+  botScore = 0;
+  tieScore = 0;
+  playerScore.innerHTML = humanScore;
+  computerScore.innerHTML = botScore;
+  tiedGame.innerHTML = tieScore;
+  resultDisplay.innerHTML = "";
+}
+
+
